8c00e47ce42811d7026ea99c50d29bd95c882b45
Mikael Sikora <Mikael.Sikora@ibm.com>
2026-07-06 16:06:24 -0700

    fixes caught by terraform plan

--- self-managed/eks/west/recreate_resources.tf
+++ self-managed/eks/west/recreate_resources.tf
@@ -1,17 +1,13 @@
-provider "aws" {
-  region = "us-west-2"
-}
-
 # Elastic IPs
 resource "aws_eip" "nat" {
-  domain = "vpc"
+  vpc = true
   tags = {
     Name = "consul-eks-ti-us-west-2a"
   }
 }
 
 resource "aws_eip" "dp" {
-  domain = "vpc"
+  vpc = true
   tags = {
     Name = "consul-eks-dp-us-west-2a"
   }
@@ -20,10 +16,10 @@ resource "aws_eip" "dp" {
 # NAT Gateway
 resource "aws_nat_gateway" "main" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = data.aws_subnet.public_a.id
+  subnet_id     = module.vpc.public_subnets[0]
 
   tags = {
-    Name = "consul-eks-ti-us-west-2a"
+    Name = "${local.name}-us-west-2a"
   }
 }
 
@@ -32,11 +28,11 @@ resource "aws_lb" "consul_ui" {
   name               = "consul-ui"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.lb.id]
-  subnets            = [data.aws_subnet.public_a.id, data.aws_subnet.public_b.id, data.aws_subnet.public_c.id]
+  security_groups    = [module.eks.node_security_group_id]
+  subnets            = module.vpc.public_subnets
 
   tags = {
-    "kubernetes.io/cluster/consul-eks-ti" = "owned"
+    "kubernetes.io/cluster/${local.name}" = "owned"
     "kubernetes.io/service-name"          = "consul/consul-ui"
   }
 }
@@ -45,51 +41,11 @@ resource "aws_lb" "api_gateway" {
   name               = "api-gateway"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [data.aws_security_group.lb.id]
-  subnets            = [data.aws_subnet.public_a.id, data.aws_subnet.public_b.id, data.aws_subnet.public_c.id]
+  security_groups    = [module.eks.node_security_group_id]
+  subnets            = module.vpc.public_subnets
 
   tags = {
-    "kubernetes.io/cluster/consul-eks-ti" = "owned"
+    "kubernetes.io/cluster/${local.name}" = "owned"
     "kubernetes.io/service-name"          = "consul/api-gateway"
-  }
-}
-
-# Data sources for existing resources
-data "aws_vpc" "main" {
-  filter {
-    name   = "tag:Name"
-    values = ["consul-eks-ti"]
-  }
-}
-
-data "aws_subnet" "public_a" {
-  vpc_id = data.aws_vpc.main.id
-  filter {
-    name   = "tag:Name"
-    values = ["consul-eks-ti-public-us-west-2a"]
-  }
-}
-
-data "aws_subnet" "public_b" {
-  vpc_id = data.aws_vpc.main.id
-  filter {
-    name   = "tag:Name"
-    values = ["consul-eks-ti-public-us-west-2b"]
-  }
-}
-
-data "aws_subnet" "public_c" {
-  vpc_id = data.aws_vpc.main.id
-  filter {
-    name   = "tag:Name"
-    values = ["consul-eks-ti-public-us-west-2c"]
-  }
-}
-
-data "aws_security_group" "lb" {
-  vpc_id = data.aws_vpc.main.id
-  filter {
-    name   = "tag:kubernetes.io/cluster/consul-eks-ti"
-    values = ["owned"]
   }
 }
